ab23fca939ae429997e07e73b5a1efbd19e8bee2
JC <nchan009@gmail.com>
2022-07-23 12:36:54 +1000

    fixed gcp build push and integration test

--- src/test/java/au/com/naythanc/controller/ExternalApiTest.java
+++ src/test/java/au/com/naythanc/controller/ExternalApiTest.java
@@ -1,12 +1,15 @@
 package au.com.naythanc.controller;
 
 import au.com.naythanc.config.WebClientConfig;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @SpringBootTest
 public class ExternalApiTest {
@@ -14,18 +17,34 @@ public class ExternalApiTest {
     @Autowired
     WebClientConfig webClientConfig;
 
-    private final String HELLO_WORLD_URL = "https://naythanc.dev/hello/api/v1/hello";
-
     @Test
     @Tag("integrationtest")
     public void helloWorldFromExternalUrl() throws Exception {
+        String helloWorldUrl = "https://naythanc.dev/hello/api/v1/hello";
         Mono<String> response = webClientConfig.getWebClient()
                                 .get()
-                                .uri(HELLO_WORLD_URL)
+                                .uri(helloWorldUrl)
                                 .retrieve()
                                 .bodyToMono(String.class);
 
-        Assertions.assertThat(response.block())
-                .contains("{ \"message\": \"Hello World\" }");
+        Assertions.assertTrue(Objects.
+                requireNonNull(response.block())
+                .contains("{ \"message\": \"Hello World\" }"));
+    }
+
+    @Test
+    @Tag("integrationtest")
+    public void errorResponseFromExternalUrl() throws Exception {
+
+        String helloWorldUrl = "https://naythanc.dev/hello/";
+        Mono<String> response = webClientConfig.getWebClient()
+                .get()
+                .uri(helloWorldUrl)
+                .retrieve()
+                .bodyToMono(String.class);
+
+        Exception exception = Assertions.assertThrows(WebClientResponseException.class, response::block);
+
+        Assertions.assertTrue(exception.getMessage().contains("404"));
     }
 }
